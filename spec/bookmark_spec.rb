4f9a153dda52372bb36ad0a433cb9238cfcf7364
kabligh <kabligh@gmail.com>
2021-02-09 20:02:41 +0000

    Add comments method to return and display comments

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -69,4 +69,15 @@ describe Bookmark do
       expect(result.url).to eq "https://www.themodernhouse.com/"
     end
   end
+
+  describe "#comments" do 
+    it 'returns a list of comments on a bookmark' do 
+      bookmark = Bookmark.create(title: 'Makers Academy', url: 'http://www.makersacademy.com')
+      DatabaseConnection.query("INSERT INTO comments (id, text, bookmark_id) VALUES(1, 'Test comment', #{bookmark.id})")
+
+      comment = bookmark.comments.first
+
+      expect(comment['text']).to eq 'Test comment'
+    end 
+  end 
 end
--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -1,10 +1,7 @@
 require 'pg'
-# require 'database_connection'
 
 def setup_test_database
   connection = PG.connect(dbname: 'bookmark_manager_test')
-
-  # Clear the bookmarks table
   result = connection.exec("SELECT table_name FROM information_schema.tables WHERE table_schema='public';")
   result.each do |row|
     connection.exec("TRUNCATE TABLE #{row["table_name"]} CASCADE")
