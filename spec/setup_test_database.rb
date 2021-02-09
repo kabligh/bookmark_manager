093b1c21f570d9d78fbddb72bc4e3fd0e2e21314
kabligh <kabligh@gmail.com>
2021-02-09 17:15:53 +0000

    Add comments table and amend db setup to truncate it with bookmarks table

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -1,8 +1,12 @@
 require 'pg'
+# require 'database_connection'
 
 def setup_test_database
   connection = PG.connect(dbname: 'bookmark_manager_test')
 
   # Clear the bookmarks table
-  connection.exec("TRUNCATE bookmarks;")
+  result = connection.exec("SELECT table_name FROM information_schema.tables WHERE table_schema='public';")
+  result.each do |row|
+    connection.exec("TRUNCATE TABLE #{row["table_name"]} CASCADE")
+  end
 end
